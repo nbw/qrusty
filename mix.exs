692310ab8574ca199eeeee9d32c9a5c1f010ab3a
Nathan Willson <willson.b.nathan@gmail.com>
2022-06-28 13:24:58 +0900

    mix/rustler precompiled 0.5.1 (#6)
    * update rustler_precomiled
    
    * update version to 0.1.5

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Qrusty.MixProject do
   use Mix.Project
 
-  @version "0.1.4"
+  @version "0.1.5"
   @source_url "https://github.com/nbw/qrusty"
 
   def project do
@@ -27,7 +27,7 @@ defmodule Qrusty.MixProject do
 
   defp deps do
     [
-      {:rustler_precompiled, "~> 0.4"},
+      {:rustler_precompiled, "~> 0.5"},
       {:rustler, ">= 0.25.0", optional: true},
 
       # docs
